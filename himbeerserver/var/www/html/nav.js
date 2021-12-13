659e7f841c91aaf23236c69533448e9bff8c641c
HimbeerserverDE <himbeerserverde@gmail.com>
2021-12-13 19:29:59 +0100

    Add empty sidebar

--- himbeerserver/var/www/html/nav.js
+++ himbeerserver/var/www/html/nav.js
@@ -1,1 +1,30 @@
+function sidebar_open() {
+	let bar = document.getElementById("sidebar");
+	bar.style.opacity = 1;
 
+	if (screen.width < 500) {
+		bar.style.width = "100%";
+	} else {
+		bar.style.width = "20%";
+	}
+
+	bar.style.animation = "sidebar_open";
+	setTimeout(_ => {
+		bar.style.animation = "";
+	}, 1000);
+}
+
+function sidebar_close() {
+	let bar = document.getElementById("sidebar");
+	bar.style.opacity = 0;
+
+	bar.style.animation = "sidebar_close";
+	setTimeout(_ => {
+		bar.style.animation = "";
+		bar.style.width = 0;
+	}, 1000);
+}
+
+function account_info() {
+	document.location.href = "/account";
+}
