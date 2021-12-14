901e50f02d19fb5b49ca18c1bb7be863e281729e
HimbeerserverDE <himbeerserverde@gmail.com>
2021-12-14 10:20:36 +0100

    Handle dpkg errors in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 dpkg-deb -b himbeerserver/
+if [ $? != 0 ]; then
+	echo -e " [\e[31m✗\e[0m] Error building deb package, see above"
+fi
+
 echo -e " [\e[32m✓\e[0m] Building done, run 'dpkg -i himbeerserver.deb' as root to install"
