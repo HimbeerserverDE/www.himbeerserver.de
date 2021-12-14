dc249f321f044a45cf2c0768d6e8ef88b8a3c006
HimbeerserverDE <himbeerserverde@gmail.com>
2021-12-14 19:03:00 +0100

    Add link to main page to sidebar

--- himbeerserver/var/www/html/nav.js
+++ himbeerserver/var/www/html/nav.js
@@ -1,20 +1,54 @@
+let pages = {
+	"Main Page": "/",
+};
+
 function sidebar_open() {
+	let container = document.getElementById("container");
 	let bar = document.getElementById("sidebar");
 	bar.style.opacity = 1;
 
 	if (screen.width < 500) {
 		bar.style.width = "100%";
+		container.style.width = 0;
 	} else {
 		bar.style.width = "20%";
+		container.style.width = "80%";
 	}
 }
 
 function sidebar_close() {
+	let container = document.getElementById("container");
 	let bar = document.getElementById("sidebar");
+
 	bar.style.opacity = 0;
 	bar.style.width = 0;
+	container.style.width = "100%";
+}
+
+function sidebar_rd(path) {
+	sidebar_close();
+	setTimeout(_ => {
+		document.location.href = path;
+	}, 1000);
 }
 
 function account_info() {
 	document.location.href = "/account";
+}
+
+let i = 1;
+for (let k in pages) {
+	let sidebar = document.getElementById("sidebar");
+	let div = document.createElement("div");
+
+	div.className = "sb_btn";
+	div.style.top = 5 + i*12.5 + "%";
+	div.innerText = k;
+
+	div.addEventListener("click", _ => {
+		sidebar_rd(pages[k]);
+	});
+
+	sidebar.appendChild(div);
+	i++;
 }
