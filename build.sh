21d2bdf57bf667784f10df5f72e50267a987a46a
HimbeerserverDE <himbeerserverde@gmail.com>
2021-12-31 15:42:18 +0100

    Fix installation on new system

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ dpkg-deb -b himbeerserver/
 if [ $? != 0 ]; then
 	echo -e " [\e[31m✗\e[0m] Error building deb package, see above"
 else
-	echo -e " [\e[32m✓\e[0m] Building done, run 'dpkg -i himbeerserver.deb' as root to install"
+	echo -e " [\e[32m✓\e[0m] Building done, run 'apt install ./himbeerserver.deb' as root to install"
 fi
